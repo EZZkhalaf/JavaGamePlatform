97acb501f0c1a5e0e888d72417afb5a6555d0b04
EZZkhalaf <ezz.awni@gmail.com>
2025-07-07 00:59:28 +0300

    applied game lloop using runnable threading

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,11 +1,43 @@
 package main;
 
-public class Game {
+public class Game implements Runnable{
     private GameWindow gameWindow;
     private GamePanel gamePanel;
+    private final int fpsSet = 120;
+    private Thread gameThread ;
+    private int frames = 0 ;
+    private long lastCheck = 0;
+
     public Game(){
         gamePanel = new GamePanel();
         gameWindow = new GameWindow(gamePanel);
         gamePanel.requestFocus();
+        startGameLoop();
+    }
+
+
+    private void startGameLoop(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+    @Override
+    public void run(){
+        double time_per_frame = 1000000000.0 / fpsSet;
+        long currentTime = System.nanoTime();
+        long lastFrame = currentTime;
+        while(true){
+            currentTime = System.nanoTime();
+            if(currentTime - lastFrame >= time_per_frame){
+                gamePanel.repaint();
+                lastFrame = currentTime;
+                frames ++;
+            }
+
+            if(System.currentTimeMillis() - lastCheck >= 1000){
+                lastCheck = System.currentTimeMillis();
+                System.out.println("FPS = " + frames);
+                frames = 0;
+            }
+        }
     }
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -5,13 +5,13 @@ import inputs.KeyboardInputs;
 import inputs.MouseInputs;
 
 import javax.swing.JPanel;
-import java.awt.Graphics;
+import java.awt.*;
 
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
-    private int xDelta = 100 , yDelta = 100;
-
+    private float xDelta = 100 , yDelta = 100;
+    private float xDir = 1f , yDir = 1f;
 
 
     public GamePanel(){
@@ -26,22 +26,38 @@ public class GamePanel extends JPanel {
 
     public  void changeXDelta(int value){
         this.xDelta += value;
-        repaint();
     }
     public  void changeYDelta(int value){
         this.yDelta += value;
-        repaint();
     }
 
 
     public void setRectPosition(int x ,int y){
         this.xDelta = x;
         this.yDelta = y;
-        repaint();
     }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.fillRect(xDelta,yDelta,50,50);
+
+        updateRectangle();
+        g.setColor(Color.blue);
+        g.fillRect((int)xDelta,(int)yDelta,50,50);
+
+
+
+//        repaint();
+    }
+
+    private void updateRectangle(){
+        xDelta += xDir;
+        if(xDelta > 400 || xDelta < 0){
+            xDir *= -1;
+        }
+
+        yDelta += yDir ;
+        if(yDelta>400 || yDelta <0){
+            yDir *= -1;
+        }
     }
 }
